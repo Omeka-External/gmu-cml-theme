b9cf9cca4c5e0e29e0739d9d406370a2ca0d665d
Nelson Amaya <nelsonamaya82@gmail.com>
2026-02-26 19:35:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,202 @@
+# CML: An Omeka S Theme
+
+A custom Omeka S theme built for the Center For Mason Legacies - George Mason University, offering a variety of customization options and a clean design, built using modular styling and a mobile-first approach, with a modern and accessible UX/UI.
+
+![CML Theme](https://github.com/Omeka-External/gmu-cml-theme/blob/master/theme.jpg?raw=true)
+
+## Installation
+
+For basic out-of-the-box use of the theme, follow the [Omeka S User Manual instructions for installing themes](https://omeka.org/s/docs/user-manual/sites/site_theme/#installing-themes).
+
+For more advanced use, such as customizing the theme with Sass, you'll need to install the tools with [NodeJS](https://nodejs.org/en/) (0.12 or greater). Navigate to your theme directory and run `npm install`.
+
+## Theme settings
+
+### General Settings
+
+- Theme's Primary Color: The color to be used as the theme's primary color. The default value is #595148.
+
+- Theme's Secondary Color: The color to be used as the theme's secondary color for backgrounds. The default value is #E2DED9.
+
+- Theme's Accent Color: The color to be used as the theme's accent color for links and buttons. The default value is #AB5D21.
+
+- Theme's complementary Color: The color to use on complementary content backgrounds. The default value is #6B7A68.
+
+### Contact Info
+- Location
+- Phone number
+- Email
+- Show contact info in Top Header and/or Footer
+
+### Header
+
+- Top Navigation Depth: Maximum number of levels to show in the site's top navigation bar. Set to 0 to show all levels.
+- Logo: A custom logo (SVG, JPG, PNG)
+
+### Banner
+- Banner image
+- Heading
+- Description
+- Button Label
+- Button Link
+- Content position
+- Banner image vertical position within the wrapper
+- Banner image horizontal position within the wrapper
+
+### Footer
+- Footer Logo
+- Footer Site description
+- Footer Menu
+- Footer Menu Title
+- Footer Menu Depth
+- Footer Content Title
+- Footer Content
+
+### Social Media
+- Facebook
+- Twitter
+- LinkedIn
+- Instagram
+- Youtube
+- Mastodon
+
+### Footer Bottom
+- Copyright
+- Terms Title
+- Terms URL
+- Privacy Policy Title
+- Privacy Policy URL
+
+### Media Settings
+- Media Caption in Viewer
+
+### Resource Tags
+- Show tags based on Resource Type and/or Class
+
+### Browse Settings
+- Layout for Browse Pages
+- Truncate Body Property
+
+## Customizing the Theme
+
+If you want to customize the site with your own CSS, the [CSS Editor](https://omeka.org/s/modules/CSSEditor/) module allows site administrators to write style overrides.
+
+For advanced CSS and Sass users, this theme includes variables and mixins for managing and extending many styles.
+
+### Sass Tasks
+
+Run these commands within the theme's root directory.
+
+* **npm run start**: While this task runs, it watches for changes to sass files and recompiles the CSS.
+* **gulp css**: This is the one-off task for compiling the current Sass/CSS.
+* **gulp css:watch**: This task watches for changes in the Sass, then compiles the CSS.
+
+### Sass File Structure
+
+```bash
+sass
+    ├── abstracts
+    │   ├── mixins
+    │   └── variables
+    │       ├── breakpoints
+    │       ├── colors
+    │       ├── layout
+    │       └── typography
+    ├── base
+    │   ├── elements
+    │   │   ├── body
+    │   │   ├── buttons
+    │   │   ├── caption
+    │   │   ├── fields
+    │   │   ├── hr
+    │   │   ├── icons
+    │   │   ├── language-tag
+    │   │   ├── links
+    │   │   ├── lists
+    │   │   ├── media
+    │   │   ├── resource-description
+    │   │   ├── resource-tag
+    │   │   ├── tables
+    │   │   ├── titles
+    │   │   └── tooltip
+    │   ├── layout
+    │   │   ├── layout
+    │   │   └── regions
+    │   └── typography
+    │       ├── copy
+    │       ├── headings
+    │       └── typography
+    ├── components
+    │   ├── accordion
+    │   ├── advanced-search
+    │   ├── annotation
+    │   ├── banner
+    │   ├── blocks
+    │   │   ├── assets
+    │   │   ├── browse-preview
+    │   │   ├── carousel
+    │   │   ├── collecting
+    │   │   ├── item-showcase
+    │   │   ├── item-with-metadata
+    │   │   ├── list-of-sites
+    │   │   ├── media-embed
+    │   │   ├── table-of-contents
+    │   │   └── timeline
+    │   ├── breadcrumbs
+    │   ├── facets
+    │   ├── footer
+    │   ├── header
+    │   │   ├── header
+    │   │   ├── search
+    │   ├── linked-resources
+    │   ├── mapping    
+    │   ├── metadata
+    │   ├── navigation
+    │   ├── pagination
+    │   ├── resources
+    │   │   ├── browse-controls
+    │   │   ├── resource-grid
+    │   │   ├── resource-list
+    │   ├── search-results
+    │   ├── uri-dereferencer
+    │   └── user-bar
+    ├── generic
+    │   ├── box-sizing
+    │   └── normalize
+    └── utilities
+        ├── accessibility
+        ├── alignments
+        └── clearfix
+```
+
+## Utility classes
+This theme offers a set of predefined utility classes that will help you to add styles to certain elements by just assigning them these classes.
+
+You can even combine multiple utility classes.
+
+- `inline`
+- `alignleft`
+- `alignright`
+- `aligncenter`
+- `alignfull`
+- `alignwide`
+- `alignnarrow`
+- `textleft`
+- `textright`
+- `textcenter`
+- `clearfix`
+- `screen-reader-text`
+
+
+## Copyright
+CML theme is Copyright © 2023-present Corporation for Digital Scholarship, Vienna, Virginia, USA http://digitalscholar.org
+
+The Corporation for Digital Scholarship distributes the Omeka source code
+under the GNU General Public License, version 3 (GPLv3). The full text
+of this license is given in the license file.
+
+The Omeka name is a registered trademark of the Corporation for Digital Scholarship.
+
+Third-party copyright in this distribution is noted where applicable.
+
+All rights not expressly granted are reserved.
---
+++ asset/js/script.js
@@ -0,0 +1,230 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const body = document.body;
+    const mainHeader = document.querySelector('.main-header');
+    const mainHeaderTopBar = document.querySelector('.main-header__top-bar');
+    const mainHeaderMainBar = document.querySelector('.main-header__main-bar');
+    const userBar = document.getElementById('user-bar');
+    const menuDrawer = document.getElementById('menu-drawer');
+    const menuToggle = document.querySelector( '.main-navigation__toggle' );
+    const mainContent = document.getElementById('content');
+    const mainFooter = document.querySelector('.main-footer');
+    let mainHeaderSearch = null;
+
+    // Scrolling Events
+
+    let lastKnownScrollPosition = 0;
+    let ticking = false;
+    let scrollDirection = 'up';
+
+    function onScroll(scrollPos) {
+        if(scrollPos > 60 && scrollDirection == 'down') {
+            mainHeader.style.top = - (userBarHeight + mainHeaderTopBar.offsetHeight) + 'px';
+            menuDrawer.style.top = mainHeaderMainBar.offsetHeight + 'px';
+            menuDrawer.style.height = 'calc(100% - ' + mainHeaderMainBar.offsetHeight + 'px)';
+        } else {
+            mainHeader.style.top = 0;
+            menuDrawer.style.top = mainHeader.offsetHeight + 'px';
+            menuDrawer.style.height = 'calc(100% - ' + mainHeader.offsetHeight + 'px)';
+        }
+    }
+
+    document.addEventListener('scroll', (event) => {
+        scrollDirection = Math.max(lastKnownScrollPosition, window.scrollY) == lastKnownScrollPosition ? 'up': 'down';
+        lastKnownScrollPosition = window.scrollY;
+
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                onScroll(lastKnownScrollPosition);
+                ticking = false;
+            });
+
+            ticking = true;
+        }
+    });
+
+    // Resize Events
+
+    let userBarHeight = 0;
+    let timeout = false;
+    const delay = 150;
+
+    onResize();
+
+    function onResize() {
+        getUserBarHeight();
+        refreshBodyPaddingTop();
+        setImageHoverTextInitialPosition();
+        setMainContentMinHeight();
+        onScroll(lastKnownScrollPosition);
+
+        if (window.innerWidth >= 1200 && menuToggle.getAttribute('aria-expanded') === 'true') {
+            menuToggle.click();
+        }
+    }
+
+    window.addEventListener('resize', function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(onResize, delay);
+    });
+
+    function refreshBodyPaddingTop() {
+        body.style.paddingTop = mainHeader.offsetHeight + 'px';
+        document.documentElement.style.scrollPaddingTop = (mainHeaderMainBar.offsetHeight + 20) + 'px';
+    }
+
+    function getUserBarHeight() {
+        if (userBar) {
+            userBarHeight = userBar.offsetHeight;
+        }
+    }
+
+    // Annotations tooltip position
+
+    const annotationBtns = document.querySelectorAll('.annotation-btn');
+
+    annotationBtns.forEach((annotationBtn) => {
+        const annotationTooltip = annotationBtn.querySelector('.annotation-tooltip');
+        const annotationTooltipWrapper = annotationTooltip.querySelector('.annotation-tooltip__wrapper');
+
+        const eventList = ['click', 'mouseover'];
+        eventList.forEach((event) => {
+            annotationBtn.addEventListener(event, setAnnotationTooltipPos);
+        });
+
+        function setAnnotationTooltipPos() {
+            const annotationBtnOffset = annotationBtn.getBoundingClientRect();
+            const { top, left } = annotationBtnOffset;
+            const distanceToRightEdge = window.innerWidth - (left + annotationBtn.offsetWidth);
+
+            if (distanceToRightEdge < (annotationTooltipWrapper.offsetWidth + 15)) {
+                annotationTooltip.style.left = (distanceToRightEdge - annotationTooltipWrapper.offsetWidth - 15) + 'px';
+            } else {
+                annotationTooltip.style.left = '0px';
+            }
+
+            if ((top - mainHeader.offsetHeight - mainHeader.offsetTop) < (annotationTooltipWrapper.offsetHeight + 15)) {
+                annotationTooltip.style.bottom = (- annotationTooltipWrapper.offsetHeight - 20) + 'px';
+                annotationTooltipWrapper.classList.add('below-button');
+            } else {
+                annotationTooltip.style.bottom = '10px';
+                annotationTooltipWrapper.classList.remove('below-button');
+
+                if (annotationTooltip.style.left == '0px') {
+                    annotationTooltip.style.bottom = '5px';
+                }
+            }
+        }
+    });
+
+    // Main Header Search
+    document.addEventListener('click', onDocumentClick, true);
+
+    function onDocumentClick(e) {
+        if (e.target.classList.contains('main-search-button')) {
+            const nextSibling = e.target.nextElementSibling;
+            console.log('nextSibling=', nextSibling);
+            if (nextSibling && nextSibling.classList.contains('main-header-search')) {
+                mainHeaderSearch = nextSibling;
+                mainHeaderSearch.classList.toggle('visible');
+                if (mainHeaderSearch.classList.contains('visible')) {
+                    const mainSearchInput = mainHeaderSearch.querySelector('input[name="fulltext_search"]');
+                    if (mainSearchInput) {
+                        mainSearchInput.focus();
+                    }
+                    document.addEventListener('focusin', onFocusInOutside, true);
+                } else {
+                    document.removeEventListener('focusin', onFocusInOutside, true);
+                }
+            }
+        } else if (mainHeaderSearch && !mainHeaderSearch.contains(e.target)) {
+            mainHeaderSearch.classList.remove('visible');
+            document.removeEventListener('focusin', onFocusInOutside, true);
+        }
+    }
+
+    function onFocusInOutside(e) {
+        if (mainHeaderSearch && !mainHeaderSearch.contains(e.target)) {
+            mainHeaderSearch.classList.remove('visible');
+            document.removeEventListener('focusin', onFocusInOutside, true);
+        }
+    }
+
+    // Forms
+    const forms = document.querySelectorAll('form');
+    forms.forEach(form => {
+        const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            if (checkbox.parentElement.classList.contains('inputs')) {
+                const previousElementSibling = checkbox.parentElement.previousElementSibling;
+                if (previousElementSibling && previousElementSibling.classList.contains('field-meta')) {
+                    checkbox.parentElement.append(previousElementSibling);
+                    checkbox.style.float = 'left';
+                    checkbox.style.marginRight = '10px';
+                }
+            }
+        });
+    });
+
+    // Tooltips
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape' || e.key === 'Esc') {
+            document
+                .querySelectorAll('.tooltip.is-visible')
+                .forEach(el => el.classList.remove('is-visible'));
+        }
+    });
+
+    document.querySelectorAll('.tooltip').forEach(tooltip => {
+        const button = tooltip.querySelector('.tooltip-button');
+
+        if (!button) {
+            return;
+        }
+
+        button.addEventListener('mouseenter', () =>
+            tooltip.classList.add('is-visible')
+        );
+        button.addEventListener('focus', () =>
+            tooltip.classList.add('is-visible')
+        );
+
+        button.addEventListener('mouseleave', () =>
+            tooltip.classList.remove('is-visible')
+        );
+        button.addEventListener('blur', () =>
+            tooltip.classList.remove('is-visible')
+        );
+    });
+
+    // Image Hover Text
+    function setImageHoverTextInitialPosition() {
+        document.querySelectorAll('.image-hover-text').forEach(function(imageHoverTextBlock) {
+            const blockHtml = imageHoverTextBlock.querySelector('.block-html');
+
+            if (!blockHtml) {
+                return;
+            }
+
+            const blockHtmlTitle = blockHtml.querySelector('.heading');
+
+            if (blockHtmlTitle) {
+                const blockHtmlTitleStyles = window.getComputedStyle(blockHtmlTitle);
+                const blockHtmlTitleMarginBottom = parseFloat(blockHtmlTitleStyles.marginBottom);
+                const blockHtmlInitialBottom = blockHtml.offsetHeight - blockHtmlTitle.offsetTop - blockHtmlTitle.offsetHeight - blockHtmlTitleMarginBottom;
+                blockHtml.style.bottom = - blockHtmlInitialBottom + 'px';
+            }
+
+        });
+    }
+
+    // Image Hover Text Read More links
+    document.querySelectorAll('.image-hover-text .read-more a').forEach(function(imageHoverTextViewMore) {
+        imageHoverTextViewMore.textContent = '';
+    });
+
+    // Set Main-content min-height
+    function setMainContentMinHeight() {
+        mainContent.style.minHeight = `calc(100vh - ${mainHeader.offsetHeight + mainFooter.offsetHeight}px)`;
+    }
+});
